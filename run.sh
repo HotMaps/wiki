56e5da8b3c9988cb39e3e4c61bceef73b72a670e
root <root@hotmapsdev.hevs.ch>
2019-12-18 11:29:57 +0100

    remove unused content

--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run -it -d --rm -p 127.0.0.1:4567:4567 --name wiki -e GITHUB_CLIENT_ID=0123456789 -e GITHUB_CLIENT_SECRET=0123456789 -e AUTH_USERS=email1,email2,email3 gollum_docker 
